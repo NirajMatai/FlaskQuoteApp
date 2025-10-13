8b1b8eefbf9d9d9c53ccc069aa31a1d420402f6b
NirajMatai <matainiraj18@gmail.com>
2025-10-13 21:03:15 +0530

    Add backend logic to fetch quote from API

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+# Import the tools we need
+from flask import Flask, render_template, request
+import requests # This is the library we use to make web requests
 
+# Initialize our Flask app
 app = Flask(__name__)
 
-@app.route('/')
+# This is the URL of the free Quote API
+QUOTE_API_URL = "https://api.quotable.io/random"
+
+# This is the main route for our webpage
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    # Set default empty values for the quote
+    quote_text = ""
+    quote_author = ""
+
+    # Check if the user clicked the button on our webpage
+    if request.method == 'POST':
+        # Make a request to the Quote API
+        response = requests.get(QUOTE_API_URL)
+        # Parse the JSON response into a Python dictionary
+        data = response.json()
+
+        # Get the quote content and author from the dictionary
+        quote_text = data['content']
+        quote_author = data['author']
+
+    # Render the HTML page, passing the quote data to it
+    return render_template('index.html', quote=quote_text, author=quote_author)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
